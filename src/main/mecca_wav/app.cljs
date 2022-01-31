f7059599bbbc604090f0e44ef6f6d4cd117bb8a7
Bobby Towers <porkostomus@mail.com>
2022-01-31 13:14:00 -0800

    process WAV header and render audio data

--- src/main/mecca_wav/app.cljs
+++ src/main/mecca_wav/app.cljs
@@ -1,14 +1,79 @@
 (ns mecca-wav.app
-  (:require [reagent.dom :as rdom]))
+  (:require 
+   [reagent.core :as r]
+   [reagent.dom :as rdom]
+   [goog.object :as o]
+   [goog.crypt :as crypt]))
 
 (defn square-root
   [x]
   (.sqrt js/Math x))
 
+(defonce file-atom (r/atom ""))
+
+(defn file-upload []
+  [:div
+   [:h1 "File upload"]
+   [:input#input
+    {:type      "file"
+     :on-change
+     (fn [e]
+       (let [dom    (o/get e "target")
+             file   (o/getValueByKeys dom #js ["files" 0])
+             reader (js/FileReader.)]
+         (.readAsArrayBuffer reader file)
+         (set! (.-onload reader)
+               #(reset! file-atom (-> % .-target .-result
+                                      (js/Uint8Array.)
+                                      crypt/byteArrayToHex
+                                      .toUpperCase
+                                      )))))}]])
+
+(defn offsets
+  ([file n] (offsets file n (inc n)))
+  ([file from to]
+   (map #(apply str %)
+        (partition 2 (take (- (* 2 to) (* 2 from))
+                           (drop (* 2 from) file))))))
+
+(defn ascii [bytes]
+  (apply str (map #(js/String.fromCharCode (str "0x" %)) bytes)))
+
+(defn decimal [bytes]
+  (js/parseInt (str "0x" (apply str (reverse bytes)))))
+
+(defn header [file]
+  {:ckID            (ascii (offsets file 0 4))
+   :cksize          (decimal (offsets file 4 8))
+   :WAVEID          (ascii (offsets file 8 12))
+   :fmtID           (ascii (offsets file 12 16))
+   :fmtsize         (decimal (offsets file 16 20))
+   :wFormatTag      (decimal (offsets file 20 22))
+   :nChannels       (decimal (offsets file 22 24))
+   :nSamplesPerSec  (decimal (offsets file 24 28))
+   :nAvgBytesPerSec (decimal (offsets file 28 32))
+   :nBlockAlign     (decimal (offsets file 32 34))
+   :wBitsPerSample  (decimal (offsets file 34 36))
+   :dataID          (ascii (offsets file 36 40))
+   :datasize        (decimal (offsets file 40 44))})
+
 (defn app []
   [:div#app
    [:h1 "mecca-wav"]
-   [:h2 "PCM data browser"]])
+   [:h2 "PCM data browser"]
+   [file-upload]
+   [:h3 "Header:"]
+   [:textarea
+    {:rows      6
+     :cols      50
+     :value     (str (header @file-atom))
+     :read-only true}]
+   [:h3 "Data:"]
+   [:textarea
+    {:rows      30
+     :cols      110
+     :value     (str @file-atom)
+     :read-only true}]])
 
 (defn render []
   (rdom/render [app]
