9de6f696bfd35f86ebc29d6de731ee6b364abef4
Bobby Towers <porkostomus@mail.com>
2022-02-01 14:54:54 -0800

    limiting range makes playback almost work

--- src/main/mecca_wav/app.cljs
+++ src/main/mecca_wav/app.cljs
@@ -7,21 +7,86 @@
 
 (defonce ^:dynamic *context* (js/AudioContext.))
 
+(defn scale
+  "Scales a sequence of values to output between t-min and t-max."
+  [values t-min t-max]
+  (let [maximum (apply max values)
+        minimum (apply min values)
+        spread (- maximum minimum)]
+    (reverse (map #(+ (* (/ (- % minimum)
+                            spread)
+                         (- t-max t-min))
+                      t-min)
+                  values))))
+
+(defn decimal [bytes]
+  (js/parseInt (str "0x" (apply str (reverse bytes)))))
+
 (defn audio-buffer [input context]
   (let [sample-rate 44100
         frame-count (count input)
         buffer (.createBuffer context 1 frame-count sample-rate)
         data (.getChannelData buffer 0)]
-    (doseq [i (range frame-count)]
+    (doseq [i (range 10000)]
       (aset data i (nth input i)))
     buffer))
 
+
+(defonce file-atom (r/atom ""))
+
+(comment
+  (nth (scale (map decimal (map #(apply str %)
+                                (partition 4 @file-atom)))
+              -1 1)
+       44000)
+
+(repeatedly 10 #(-> (js/Math.random) (* 2.0) (- 1.0)) )
+
+(let [duration 1.0
+      context *context*
+      sample-rate 44100
+      frame-count (* sample-rate duration)
+      buffer (.createBuffer context 1 frame-count sample-rate)
+      data (.getChannelData buffer 0)
+      wav (scale (map decimal (map #(apply str %)
+                                   (partition 4 @file-atom)))
+                 -1 1)]
+ (doseq [i (range 10000)]
+      (aset data i (nth wav i)))
+(buffer-source buffer)
+  )
+  
+
+  (let [context *context*
+        input (scale (map decimal (map #(apply str %)
+                                       (partition 4 @file-atom)))
+                     -1 1)
+        sample-rate 44100
+        frame-count (count input)
+        buffer (.createBuffer context 1 frame-count sample-rate)
+        data (.getChannelData buffer 0)]
+    (doseq [i (range frame-count)]
+      (aset data i (nth input i)))
+   (buffer-source buffer))
+  )
+
+
+
+#_(defn audio-buffer [context duration]
+  (let [sample-rate 44100
+        frame-count (* sample-rate duration)
+        buffer (.createBuffer context 1 frame-count sample-rate)
+        data (.getChannelData buffer 0)]
+    (doseq [i (range frame-count)]
+      (aset data i (-> (js/Math.random) (* 2.0) (- 1.0))))
+    buffer))
+                                    
 (defn buffer-source [buffer]
   (let [source (.createBufferSource *context*)
         gain (.createGain *context*)]
     (.resume *context*)
     (set! (.-buffer source) buffer)
-    (.setValueAtTime (.-gain gain) 0.5 (.-currentTime *context*))
+    (.setValueAtTime (.-gain gain) 0.1 (.-currentTime *context*))
     (.connect source gain)
     (.connect gain (.-destination *context*))
     (.start source (.-currentTime *context*))
@@ -33,25 +98,21 @@
   [x]
   (.sqrt js/Math x))
 
-(defonce file-atom (r/atom ""))
 
 (defn file-upload []
-  [:div
-   [:h1 "File upload"]
-   [:input#input
-    {:type      "file"
-     :on-change
-     (fn [e]
-       (let [dom    (o/get e "target")
-             file   (o/getValueByKeys dom #js ["files" 0])
-             reader (js/FileReader.)]
-         (.readAsArrayBuffer reader file)
-         (set! (.-onload reader)
-               #(reset! file-atom (-> % .-target .-result
-                                      (js/Uint8Array.)
-                                      crypt/byteArrayToHex
-                                      .toUpperCase
-                                      )))))}]])
+  [:input#input
+   {:type      "file"
+    :on-change (fn [e]
+                 (let [dom    (o/get e "target")
+                       file   (o/getValueByKeys dom #js ["files" 0])
+                       reader (js/FileReader.)]
+                   (.readAsArrayBuffer reader file)
+                   (set! (.-onload reader)
+                         #(reset! file-atom (-> % .-target .-result
+                                                (js/Uint8Array.)
+                                                crypt/byteArrayToHex
+                                                .toUpperCase
+                                                )))))}])
 
 (defn offsets
   ([file n] (offsets file n (inc n)))
@@ -63,8 +124,11 @@
 (defn ascii [bytes]
   (apply str (map #(js/String.fromCharCode (str "0x" %)) bytes)))
 
-(defn decimal [bytes]
-  (js/parseInt (str "0x" (apply str (reverse bytes)))))
+
+(js/parseInt (str "0x" (apply str (reverse "0100"))))
+(decimal "0100")
+
+(decimal (offsets @file-atom 24 28))
 
 (defn header [file]
   {:ckID            (ascii (offsets file 0 4))
@@ -89,25 +153,16 @@
      :x            x
      :y            y
      :stroke       "#00d0ff"
-     :stroke-width 0.05}])
-
-(let [items '(-1 -1 -1 1 1 1)
-      bar-width (/ 500 (count items))]
-  (for [bar (range (count items))]
-    (svg-bar bar-width
-             (+ (nth items bar) 1)
-             (* bar bar-width)
-             (- 50 (/ (nth items bar) 2.0))
-             "magenta")))
+     :stroke-width 0.5}])
 
 (defn bars [items]
-  (let [bar-width (/ 100 (count items))]
+  (let [bar-width (/ 500 (count items))]
     (into [:g]
           (for [bar (range (count items))]
             (svg-bar bar-width
-                     1
+                     2
                      (* bar bar-width)
-                     (- 20 (* 10 (nth items bar)))
+                     (- 50 (* 2 (nth items bar)))
                       "magenta")))))
 
 (bars '(-1 -1 -1 1 1 1))
@@ -120,49 +175,59 @@
    {:on-click onclick}
    label])
 
-(defn scale2 [values]
-  (let [maximum (apply max values)
-        minimum (apply min values)
-        spread (- maximum minimum)]
-    (map #(- (* 2 (/ (- % minimum) spread)) 1)
-    values)))
 
-(subs  "ehtdsrfo" 1)
+
+(defn make-path [points]
+  (str "M" (apply str (interpose " " (for [x (range (count points))]
+                                       (str x " " (nth points x)))))))
+
+(comment
+  
+  )
+
+
 
 (defn app []
   [:div#app
-   [:h1 "mecca-wav"]
-   [:h2 "PCM data browser"]
    [file-upload]
    [:div [button "Play"
-        (fn [] (buffer-source (audio-buffer (map decimal (map #(apply str %)
-                                                              (partition 4 @file-atom)))
-                                            *context*)))]]
+        (fn [] (buffer-source (audio-buffer 
+                               (scale (map decimal (map #(apply str %)
+                                                                     (partition 4 @file-atom)))
+                                                   -1 1)
+                                            *context*
+                               )))]]
+   [:p]
    [:svg {:width    "100%"
-          :view-box (str "0 0 100 50")}
-    [:path {:d (let [data (let [data  (scale2 (drop 44 (map decimal (map #(apply str %)
-                                                                              (partition 4 @file-atom)))))
-                                     parts (.floor js/Math (/ (count data) 500))]
-                                 (map first (partition parts data)))]
-                      (str "M" (subs (apply str (for [x (range (count data))]
-                                                  (make-path-data x (- 20 (* 10 (nth data x))))))
-                                     1)))
-            :stroke "black"
-            :stroke-width 0.1
-            :fill "none"}]]
+          :view-box (str "0 0 500 150")}
+    [:path {:d            (let [data (let [data  (scale (take 10000 (drop 44 (map decimal (map #(apply str %)
+                                                                                             (partition 4 @file-atom)))))
+                                                        -1 1)
+                                           parts (.floor js/Math (/ (count data) 500))]
+                                       (map first (partition parts data)))]
+                            (make-path (scale data 0 150) ))
+            :stroke       "black"
+            :stroke-width 0.5
+            :fill         "none"}]]
    [:h3 "Header:"]
    [:textarea
     {:rows      6
      :cols      50
      :value     (str (header @file-atom))
      :read-only true}]
-   [:h3 "Data:"]
+   [:h3 "Data (hex):"]
    [:textarea
-    {:rows      30
+    {:rows      15
      :cols      110
-     :value     (str (map decimal (map #(apply str %)
-                                       (partition 4 @file-atom))))
-     :read-only true}]])
+     :value     (str (map #(apply str %)(partition 4 @file-atom)))
+     :read-only true}]
+   [:h3 "Data (decimal):"]
+[:textarea
+ {:rows      15
+  :cols      110
+  :value     (str (map decimal (map #(apply str %)
+                                    (partition 4 @file-atom))))
+  :read-only true}]])
 
 (defn render []
   (rdom/render [app]
